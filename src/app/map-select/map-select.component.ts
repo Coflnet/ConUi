9a76a61f723e069800b6a0cc2209d17fa8ddefac
Äkwav <ekwav@coflnet.com>
2024-10-29 11:49:46 +0100

    persist last area as its the most relevant

--- src/app/map-select/map-select.component.ts
+++ src/app/map-select/map-select.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
 import * as L from 'leaflet';
 
+/**
+ * This component only renders in the browser and initializes a map with OpenStreetMap tiles.
+ */
 @Component({
   selector: 'app-map-select',
   standalone: true,
@@ -18,11 +20,13 @@ export class MapSelectComponent {
 
   private initMap(): void {
     // Initialize the map and set its view to center of Germany
-    this.map = L.map('map').setView([49.1124747, 12.6696695], 9);
+    let mapCenter = JSON.parse(localStorage.getItem('mapCenter') || 'null') ?? [49.1124747, 12.6696695];
+    let mapZoom = JSON.parse(localStorage.getItem('mapZoom') || 'null') ?? 9;
+    this.map = L.map('map').setView(mapCenter, mapZoom);
 
     // Set up the tile layer from OpenStreetMap
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 20,
+      maxZoom: 19,
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
     
@@ -32,6 +36,15 @@ export class MapSelectComponent {
         .addTo(this.map!)
         .bindPopup('You clicked the map at ' + e.latlng.toString())
         .openPopup();
+    });
+
+    this.map.on('moveend', () => {
+      console.log(this.map!.getCenter());
+      localStorage.setItem('mapCenter', JSON.stringify(this.map!.getCenter()));
+    });
+    this.map.on('zoomend', () => {
+      console.log(this.map!.getZoom());
+      localStorage.setItem('mapZoom', JSON.stringify(this.map!.getZoom()));
     });
   }
 }
